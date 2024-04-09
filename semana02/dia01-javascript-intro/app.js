e52b611f70a1ed29f8cad2f8f1d19467c249794f
Victor Villazón <victorzn23@gmail.com>
2024-04-08 20:35:40 -0500

    semana02, dia01, javascript intro

--- semana02/dia01-javascript-intro/app.js
+++ semana02/dia01-javascript-intro/app.js
@@ -84,3 +84,82 @@ edad2 = true
 edad2 = 22
 
 console.log(edad2, typeof edad2)
+
+// OPERADORES MATEMÁTICOS
+
+console.log(1 + 2)
+console.log(3 - 2)
+console.log(2 * 2)
+console.log(1 / 2)
+console.log(1 % 2) // Residuo
+console.log(2 ** 3) // Dos elevado al cubo
+console.log(Math.pow(2, 3)) // Hace lo mismo que 2 ** 3
+
+// OPERADORES DE ASIGNACIÓN
+
+const genero = 'femenino'
+
+console.log(genero)
+
+// OPERADORES DE COMPARACIÓN
+
+// Operador de igualdad y desigualdad no estricta (==)
+// Devuelve un valor booleano (true o false)
+// Este oprador no toma en cuenta el tipo de datos al comparar sino solo sus valores
+
+console.log(1 == 1) // true
+console.log(1 == '1') // true
+console.log(1 != 1) // false
+
+// Operador de igualdad y desigualdad estricta (===) (✅ ES RECOMENDABLE USARLO CASI SIEMPRE)
+// Devuelve un valor booleano (true o false)
+// Este operador si toma en cuenta el tipo de dato y sus valores al comparar
+
+console.log(1 === 1) // true
+console.log(1 === '1') // false
+console.log(1 !== 1) // false
+
+// OPERADORES DE COMPARACIÓN, siempre devuelven un boolean
+
+console.log(8 > 5) // true
+console.log(5 < 1) // false
+console.log(8 >= 5) // true
+console.log(8 <= 5) // false
+
+// OPERADORES LÓGICOS (and, or, negación)
+
+console.log(true && false) // AND
+console.log(true || false) // OR
+console.log(!false) // NEGACIÓN
+
+// Operador de cadena (concatenación)
+
+const saludo = 'Hola, '
+const nombreCompleto = 'Victor' + ' ' + 'Villazón'
+const miEdad = 37
+
+const saludoAmigo = saludo + nombreCompleto + ". Tengo " + miEdad + ' años'
+
+console.log(nombreCompleto)
+console.log(saludoAmigo)
+
+// EJERCICIOS (TODO)
+
+// 1. Retorna true si dos strings tienen la misma longitud sino devolver false
+
+const cadena1 = 'codigo'
+const cadena2 = 'cadaga'
+
+console.log(cadena1.length)
+console.log(cadena2.length)
+
+console.log(cadena1.length === cadena2.length)
+
+// 2. Retornar true si un numero es menor que 40 sino devolver false
+// 3. Retornar True si un numero es menor que 60 sino devolver False
+// 4. Retornar True si un numero es par sino devolver False
+// 5. Retornar True si un numero es impar sino devolver False
+// 6. calcular el area de un triangulo y mostrar su resultado si tenemos los valores base y altura
+
+// 7. Almacenar en una constante un número de 3 cifras y mostrar la suma de sus cifras elevada al cubo.
+// 8. Almacenar en una constante un monto de dinero, luego mostrar cuanto le toca a cada socio según la siguiente tabla: SOCIO A = 30%, SOCIO B = 20%, SOCIO C = 60%
