1fc3df4fe9c73590c8f5e34b09406574a8ebec0e
Victor Villazón <victorzn23@gmail.com>
2024-04-12 21:47:24 -0500

    semana02, dia03, javascript countries modules

--- semana02/dia03-javascript-fetch-countries/modules/main.js
+++ semana02/dia03-javascript-fetch-countries/modules/main.js
@@ -1,1 +1,20 @@
 console.log('JS ❤')
+
+const url = 'https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population,languages,currencies,timezones'
+
+const fetchCountries = async () => {
+  try {
+    const response = await fetch(url) // Promise
+
+    if (!response.ok) {
+      throw new Error('ERROR', response.status)
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchCountries()
+  .then(countries => console.log(countries))
