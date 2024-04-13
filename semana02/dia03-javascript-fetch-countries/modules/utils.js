f15fbe9adb1b47fb02f55cb642d24626384e4846
Victor Villazón <victorzn23@gmail.com>
2024-04-12 22:49:02 -0500

    semana02, dia03, javascript countries modules

--- semana02/dia03-javascript-fetch-countries/modules/utils.js
+++ semana02/dia03-javascript-fetch-countries/modules/utils.js
@@ -7,7 +7,21 @@ export const renderCountries = (countries = []) => {
 
   countries.forEach(country => {
     countryList += `
-      <h1>${country.name.common}</h1>
+      <div class="country">
+        <img class="country__flag" src="${country.flags.png}" alt="${country.flags.alt}" />
+        <div class="country__wrapper">
+          <h2>${country.name.common}</h2>
+          <div class="country__description">
+            <strong>Population:</strong> ${country.population}
+          </div>
+          <div class="country__description">
+            <strong>Region:</strong> ${country.region}
+          </div>
+          <div class="country__description">
+            <strong>Capital:</strong> ${country.capital[0]}
+          </div>
+        </div>
+      </div>
     `
   })
 
