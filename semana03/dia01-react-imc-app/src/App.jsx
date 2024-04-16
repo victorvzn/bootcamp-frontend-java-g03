5e82acbe4f08da1f54029d4e2f768ea276962c83
Victor Villazón <victorzn23@gmail.com>
2024-04-15 22:28:49 -0500

    semana03, dia01, react imc app

--- semana03/dia01-react-imc-app/src/App.jsx
+++ semana03/dia01-react-imc-app/src/App.jsx
@@ -1,6 +1,85 @@
+import { useState } from "react"
+
 function App() {
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [result, setResult] = useState(0)
+  const [resultValue, setResultValue] = useState('')
+
+  const handleWeight = (event) => {
+    const value = event.target.value
+
+    setWeight(value)
+
+    setResult(weight / ((height / 100) ** 2))
+
+    bodyMassIndexResult()
+  }
+
+  const handleHeight = (event) => {
+    const value = event.target.value
+
+    setHeight(value)
+
+    setResult(weight / ((height / 100) ** 2))
+
+    bodyMassIndexResult()
+  }
+
+  const bodyMassIndexResult = () => {
+    let result2 = ''
+
+    if (result < 18.5) {
+      result2 = 'Baja'
+    } else if (result >= 18.5 && result <= 24.9) {
+      result2 = 'Normal'
+    } else if (25 < result < 29.9) {
+      result2 = 'Sobrepeso'
+    } else if (result > 30) {
+      result2 = 'Obeso'
+    } else {
+      result2 = 'Datos incorrectos'
+    }
+
+    setResultValue(result2)
+  }
+
   return (
-    <h1 className="text-3xl text-red-600 font-bold text-center">IMC Calculator</h1>
+    <section>
+      <h1 className="text-3xl text-red-600 font-bold text-center">
+        IMC Calculator
+      </h1>
+      <div>
+        <p>Weight: {weight} kg</p>
+        <input
+          type="range"        
+          min="50"
+          max="200"
+          onChange={handleWeight}
+          value={weight}
+        />
+      </div>
+
+      <div>
+        <p>Height: {height} cm</p>
+        <input
+          type="range"        
+          min="50"
+          max="200"
+          onChange={handleHeight}
+          value={height}
+        />
+      </div>
+
+      <p>
+        Tu IMC es {result}
+      </p>
+
+      <p>
+        Estado de IMC: {resultValue}
+      </p>
+
+    </section>
   )
 }
 
