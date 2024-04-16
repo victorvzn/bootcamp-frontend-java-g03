a003ec80b3241f83591cb536fd3fb44ab663165d
Victor Villazón <victorzn23@gmail.com>
2024-04-15 22:50:42 -0500

    semana03, dia01, react todo app

--- semana03/dia01-react-todo-app/src/App.jsx
+++ semana03/dia01-react-todo-app/src/App.jsx
@@ -1,34 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const DEFAULT_TODOS = [
+    {
+      id: 1,
+      title: 'Aprender JS',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Aprender CSS',
+      completed: true
+    },
+    {
+      id: 3,
+      title: 'Aprender React.js',
+      completed: false
+    }
+  ]
+
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main
+      className="bg-yellow-100 w-full max-w-sm mx-auto mt-10 border border-yellow-600 rounded-lg shadow-lg p-4"
+    >
+      <h1 className="text-2xl font-bold text-center">TODO APP</h1>
+
+      {/* {JSON.stringify(todos)} */}
+
+      <form>
+        <input
+          className="w-full border my-3 p-3"
+          type="text"
+          placeholder="¿Qué deseas hacer hoy?"
+          required
+        />
+      </form>
+
+      <div className="flex justify-between items-center">
+        <span className="font-bold">2 de 3</span>
+        <button
+          className="bg-blue-500 rounded-lg px-2 py-1 text-white hover:bg-blue-700 duration-300"
+        >
+          Limpiar tareas completadas
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <section className="mt-8">
+        <ul className="flex flex-col gap-2">
+          <li className="flex">
+            <input
+              className="mr-2"
+              type="checkbox"
+            />
+            <div className="w-full flex justify-between items-center">
+              <div>
+                Task name
+              </div>
+              <button
+                className="bg-red-300 rounded-lg px-2 py-2"
+              >
+                ❌
+              </button>
+            </div>
+          </li>
+          <li className="flex">
+            <input
+              className="mr-2"
+              type="checkbox"
+            />
+            <div className="w-full flex justify-between items-center">
+              <div>
+                Task name
+              </div>
+              <button
+                className="bg-red-300 rounded-lg px-2 py-2"
+              >
+                ❌
+              </button>
+            </div>
+          </li>
+          <li className="flex">
+            <input
+              className="mr-2"
+              type="checkbox"
+              checked
+            />
+            <div className="w-full flex justify-between items-center">
+              <div className="line-through">
+                Task name
+              </div>
+              <button
+                className="bg-red-300 rounded-lg px-2 py-2"
+              >
+                ❌
+              </button>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </main>
   )
 }
 
