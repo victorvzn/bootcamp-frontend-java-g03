020ef0738bb71d0759b8e49bc9817e4282de532f
Victor Villazón <victorzn23@gmail.com>
2024-04-15 20:27:17 -0500

    semana03, dia01, react introduction

---
+++ semana03/dia01-react-intro/src/App.jsx
@@ -0,0 +1,93 @@
+// COMPONENTE USANDO FUNCTION
+
+// function App() {
+//   return (
+//     <h1>Hola React.js</h1>
+//   )
+// }
+
+// export default App
+
+// COMPONENTE USANDO ARROW FUNCTIONS con React Fragments
+
+// const App = () => {
+//   // const xx = 'hola'
+
+//   return (
+//     <>
+//       {/* <h1>Hola React.js</h1> */}
+//       <h2>Hola Vite</h2>
+//     </>
+//   )
+// }
+
+// COMPONENTES DE REACT SUSANDO MÚLTIPLES LÍNEAS CON SU ELEMENTO PADRE
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Hola React!</h1>
+//       <p>Estoy en el bootcamp!</p>
+//     </div>
+//   )
+// }
+
+// COMPONENTE USANDO FRAGMENTOS (FRAGMENTS) PARA EVITAR RENDERIZAR EN UN ELEMENTO PADRE
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hola React!</h1>
+//       <p>Estoy en el bootcamp!</p>
+//     </>
+//   )
+// }
+
+// export default App
+
+// SNIPPETS PARA CREAR COMPONENTES
+
+// RFCE Y RFC
+
+// ANINDANDO COMPONENTES DENTRO DE OTROS
+
+// function Saludo() {
+//   return <h4>Hola Victor</h4>
+// }
+
+// function Despedida() {
+//   return <h4>Adios Victor</h4>
+// }
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hola React!</h1>
+//       <p>Estoy en el bootcamp!</p>
+
+//       <Saludo />
+//       <Despedida />
+//     </>
+//   )
+// }
+
+// export default App
+
+// IMPORTANDO COMPONENTES EXTERNOS
+
+import Saludo from "./components/Saludo"
+import Despedida from "./components/Despedida"
+
+const App = () => {
+  return (
+    <>
+      <h1>Hola React!</h1>
+      <p>Estoy en el bootcamp!</p>
+
+      <Saludo />
+      <Despedida />
+    </>
+  )
+}
+
+export default App
