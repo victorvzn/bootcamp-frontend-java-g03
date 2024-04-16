8af6ab2126c85ade736647bc7320a684c7258a02
Victor Villazón <victorzn23@gmail.com>
2024-04-15 20:39:30 -0500

    semana03, dia01, react introduction

--- semana03/dia01-react-intro/src/App.jsx
+++ semana03/dia01-react-intro/src/App.jsx
@@ -75,17 +75,50 @@
 
 // IMPORTANDO COMPONENTES EXTERNOS
 
+// import Saludo from "./components/Saludo"
+// import Despedida from "./components/Despedida"
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hola React!</h1>
+//       <p>Estoy en el bootcamp!</p>
+
+//       <Saludo />
+//       <Despedida />
+//     </>
+//   )
+// }
+
+// export default App
+
+// EXPRESIONES EN UN COMPONENTE
+
 import Saludo from "./components/Saludo"
 import Despedida from "./components/Despedida"
 
+import { LISTA_DE_FRUTAS, EDAD } from './components/frutas'
+
 const App = () => {
+  const nombre = 'Victor'
+
   return (
     <>
       <h1>Hola React!</h1>
       <p>Estoy en el bootcamp!</p>
 
+      {/* Comentario en react dentro de JSX */}
+
       <Saludo />
       <Despedida />
+
+      {1 + 2}
+
+      {`HOLA ${nombre}`}
+
+      {LISTA_DE_FRUTAS}
+
+      {EDAD}
     </>
   )
 }
