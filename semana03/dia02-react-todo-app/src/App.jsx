ee2787b71ceafc4afa1a86e20c9cd91dbeedc034
Victor Villazón <victorzn23@gmail.com>
2024-04-17 20:17:42 -0500

    semana03, dia02, react todo app (continuación)

--- semana03/dia02-react-todo-app/src/App.jsx
+++ semana03/dia02-react-todo-app/src/App.jsx
@@ -21,24 +21,39 @@ const App = () => {
 
   const [todos, setTodos] = useState(INITIAL_TODOS)
 
-  console.log(todos)
-
   return (
-    <>
-      <h1 className="text- text-center text-red-500">App</h1>
+    <main
+      className="bg-yellow-100 w-full max-w-sm mx-auto mt-10 border border-yellow-600 rounded-lg shadow-lg p-4"
+    >
+      <h1 className="text-2xl font-bold text-center">App</h1>
 
       {/* <pre>{JSON.stringify(todos, null, 2)}</pre> */}
 
-      <section>
-        <ul>
+      <section className="mt-8">
+        <ul className="flex flex-col gap-2">
           {todos.map(todo => {
             return (
-              <li>{todo.title}</li>
+              <li className="flex">
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                />
+                <div className="w-full flex justify-between items-center">
+                  <span>
+                    {todo.title}
+                  </span>
+                  <button
+                    className="bg-red-300 rounded-lg px-2 py-2"
+                  >
+                    ❌
+                  </button>
+                </div>
+              </li>
             )
           })}
         </ul>
       </section>
-    </>
+    </main>
   )
 }
 
