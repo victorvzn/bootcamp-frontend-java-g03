765f6a5bf7c2ecdb87f98b5efb9b2ac9b7ad5df4
Victor Villazón <victorzn23@gmail.com>
2024-04-17 19:48:15 -0500

    semana03, dia02, react todo app (continuación)

--- semana03/dia02-react-todo-app/src/App.jsx
+++ semana03/dia02-react-todo-app/src/App.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react"
+
 const App = () => {
+  const INITIAL_TODOS = [
+    {
+      id: '1',
+      title: 'Aprender JS',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Aprender CSS',
+      completed: true
+    },
+    {
+      id: '3',
+      title: 'Aprender React.js',
+      completed: false
+    }
+  ]
+
+  const [todos, setTodos] = useState(INITIAL_TODOS)
+
+  console.log(todos)
+
   return (
-    <h1 className="text-6xl text-center">App</h1>
+    <>
+      <h1 className="text- text-center text-red-500">App</h1>
+
+      {JSON.stringify(todos)}
+
+
+    </>
   )
 }
 
