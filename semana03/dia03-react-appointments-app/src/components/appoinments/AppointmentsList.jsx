554166fa22edd925b4e47d262a3eb4833e34a4ff
Victor Villazón <victorzn23@gmail.com>
2024-04-19 22:56:29 -0500

    semana03, dia03, react appointments app

--- semana03/dia03-react-appointments-app/src/components/appoinments/AppointmentsList.jsx
+++ semana03/dia03-react-appointments-app/src/components/appoinments/AppointmentsList.jsx
@@ -1,9 +1,65 @@
 const AppointmentsList = ({ appointments }) => {
+
+  if (appointments.length === 0) {
+    return (
+      <section className="w-1/2 p-4 bg-white rounded-lg">
+          <h2 className="text-2xl text-center mb-4">Listado de Citas</h2>
+
+          <div className="text-2xl text-center">
+            No hay citas.
+          </div>
+
+      </section>
+    )
+  }
+
   return (
     <>
-      <div>AppointmentsList</div>
+      <section className="w-1/2 p-4 bg-white rounded-lg">
+        <h2 className="text-2xl text-center mb-4">Listado de Citas</h2>
 
-      <pre>{JSON.stringify(appointments, null, 2)}</pre>
+        {appointments.map(appointment => {
+          return (
+            <div className="flex flex-col gap-3 mb-4" key={appointment.id}>
+              <div className="border-2 border-sky-500 p-3 rounded-lg bg-sky-300">
+
+                <div>
+                  <h4 className="font-semibold text-xl">Mascota</h4>
+                  <div>
+                    <strong>Nombre:</strong> {appointment.petName}
+                  </div>
+                  <div>
+                    <strong>Edad (años):</strong> {appointment.petAge}
+                  </div>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold text-xl">Dueño</h4>
+                  <div>
+                    <strong>Nombre:</strong> {appointment.ownerName}
+                  </div>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold text-xl">Cita</h4>
+                  <div>
+                    <strong>Fecha:</strong> {appointment.appointmentDate}
+                  </div>
+                  <div>
+                    <strong>Hora:</strong> {appointment.appointmentTime}
+                  </div>
+                  <div>
+                    <strong>Síntomas:</strong> {appointment.symptoms}
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          )
+        })}
+      </section>
+
+      {/* <pre>{JSON.stringify(appointments, null, 2)}</pre> */}
     </>
   )
 }
