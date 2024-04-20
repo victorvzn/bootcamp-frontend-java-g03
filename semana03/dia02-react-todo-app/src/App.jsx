a4401f7cba0e58a92c41de99b53d68e98dc763d6
Victor Villazón <victorzn23@gmail.com>
2024-04-19 19:49:24 -0500

    semana03, dia03, react todo app (continuación)

--- semana03/dia02-react-todo-app/src/App.jsx
+++ semana03/dia02-react-todo-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 import Header from "./components/Header"
 import TodoList from "./components/TodoList"
+import Form from "./components/Form"
 
 const App = () => {
   const INITIAL_TODOS = [
@@ -23,33 +24,11 @@ const App = () => {
   ]
 
   const [todos, setTodos] = useState(INITIAL_TODOS)
-  const [input, setInput] = useState('')
 
-  const handleChange = (event) => {
-    const value = event.target.value
-
-    setInput(value)
-  }
-
-  const handleSubmit = (event) => {
-    // Evita que se actualice la página
-    event.preventDefault();
-
-    const newTodo = {
-      id: crypto.randomUUID(),
-      title: input,
-      completed: false
-    }
-
+  const handleSubmit = (newTodo) => {
     const updatedTodos = [...todos, newTodo]
 
     setTodos(updatedTodos) // <- Nos ayuda a insertar un nuevo todo
-
-    // DONE: Limpiar la caja de texto después de añadir un nuevo todo
-    setInput('')
-
-    // console.log(input)
-    // console.log('Agregando nueva tarea...')
   }
 
   const handleRemoveTodo = (event) => {
@@ -94,24 +73,9 @@ const App = () => {
     >
       <Header />
 
-      <form
-        className="flex items-center gap-2"
+      <Form
         onSubmit={handleSubmit}
-      >
-        <input
-          className="w-full border my-3 p-3"
-          type="text"
-          placeholder="¿Qué deseas hacer hoy?"
-          required
-          onChange={handleChange}
-          value={input}
-        />
-        <input
-          className="bg-blue-300 font-bold rounded-lg px-2 py-2 cursor-pointer"
-          type="submit"
-          value="Añadir"
-        />
-      </form>
+      />
 
       {/* TODO: RETO1 - Añadir una estadística de cuantas tareas estan completadas */}
       {/* TODO: RETO2 - Completar la funcionalidad del botón limpiar tareas */}
--- semana03/dia02-react-todo-app/src/components/Form.jsx
+++ semana03/dia02-react-todo-app/src/components/Form.jsx
@@ -1,6 +1,52 @@
-const Form = () => {
+import { useState } from "react"
+
+const Form = ({ onSubmit }) => {
+  const [input, setInput] = useState('')
+
+  const handleChange = (event) => {
+    const value = event.target.value
+
+    setInput(value)
+  }
+
+  const handleSubmit = (event) => {
+    // Evita que se actualice la página
+    event.preventDefault();
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: input,
+      completed: false
+    }
+
+    onSubmit(newTodo)
+
+    // DONE: Limpiar la caja de texto después de añadir un nuevo todo
+    setInput('')
+
+    // console.log(input)
+    // console.log('Agregando nueva tarea...')
+  }
+
   return (
-    <div>Form</div>
+    <form
+      className="flex items-center gap-2"
+      onSubmit={handleSubmit}
+    >
+      <input
+        className="w-full border my-3 p-3"
+        type="text"
+        placeholder="¿Qué deseas hacer hoy?"
+        required
+        onChange={handleChange}
+        value={input}
+      />
+      <input
+        className="bg-blue-300 font-bold rounded-lg px-2 py-2 cursor-pointer"
+        type="submit"
+        value="Añadir"
+      />
+    </form>
   )
 }
 
