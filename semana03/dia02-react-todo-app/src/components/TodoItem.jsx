16d221fa08b457a0cafad870fb6d377d63717765
Victor Villazón <victorzn23@gmail.com>
2024-04-19 19:28:35 -0500

    semana03, dia03, react todo app (continuación)

--- semana03/dia02-react-todo-app/src/components/TodoItem.jsx
+++ semana03/dia02-react-todo-app/src/components/TodoItem.jsx
@@ -1,6 +1,28 @@
-const TodoItem = () => {
+const TodoItem = ({ todo, onCompleted, onRemoveTodo }) => {
   return (
-    <div>TodoItem</div>
+    <li className="flex">
+      <input
+        className="mr-2"
+        type="checkbox"
+        data-id={todo.id}
+        onChange={onCompleted}
+        checked={todo.completed}
+      />
+      <div className="w-full flex justify-between items-center">
+        <span
+          className={todo.completed ? 'line-through' : ''}
+        >
+          {todo.title}
+        </span>
+        <button
+          className="bg-red-300 rounded-lg px-2 py-2"
+          data-id={todo.id}
+          onClick={onRemoveTodo}
+        >
+          ❌
+        </button>
+      </div>
+    </li>
   )
 }
 
--- semana03/dia02-react-todo-app/src/components/TodoList.jsx
+++ semana03/dia02-react-todo-app/src/components/TodoList.jsx
@@ -1,31 +1,16 @@
+import TodoItem from "./TodoItem"
+
 const TodoList = ({ todos, onCompleted, onRemoveTodo }) => {
   return (
     <ul className="flex flex-col gap-2">
       {todos?.map(todo => {
         return (
-          <li className="flex" key={todo.id}>
-            <input
-              className="mr-2"
-              type="checkbox"
-              data-id={todo.id}
-              onChange={onCompleted}
-              checked={todo.completed}
-            />
-            <div className="w-full flex justify-between items-center">
-              <span
-                className={todo.completed ? 'line-through' : ''}
-              >
-                {todo.title}
-              </span>
-              <button
-                className="bg-red-300 rounded-lg px-2 py-2"
-                data-id={todo.id}
-                onClick={onRemoveTodo}
-              >
-                ❌
-              </button>
-            </div>
-          </li>
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onCompleted={onCompleted}
+            onRemoveTodo={onRemoveTodo}
+          />
         )
       })}
     </ul>
