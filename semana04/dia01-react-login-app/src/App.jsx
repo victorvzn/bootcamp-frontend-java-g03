eee46a27847699f8d8515a9ea52e55f5066dcea4
Victor Villazón <victorzn23@gmail.com>
2024-04-22 22:37:27 -0500

    semana04, dia01, react login  app (fetch, method post, jwt)

--- semana04/dia01-react-login-app/src/App.jsx
+++ semana04/dia01-react-login-app/src/App.jsx
@@ -1,10 +1,30 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+    <main>
+      <h1 className="text-3xl font-bold underline text-center">
+        Login
       </h1>
-    </>
+
+      <form className="flex flex-col gap-2 p-8">
+        <input
+          type="text"
+          className="border p-3 shadow-sm rounded-md"
+          placeholder="jhondoe@gmail.com"
+          name="email"
+        />
+        <input
+          type="password"
+          className="border p-3 shadow-sm rounded-md"
+          placeholder="Ej. supersecret"
+          name="password"
+        />
+        <input
+          type="submit"
+          className="border p-3 bg-sky-700 text-white rounded-md cursor-pointer"
+          value="Login"
+        />
+      </form>
+    </main>
   )
 }
 
