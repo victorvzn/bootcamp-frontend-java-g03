c172d44dcabee9fc0a137daf636e670d69a7facc
Victor Villazón <victorzn23@gmail.com>
2024-04-22 20:40:40 -0500

    semana04, dia01, react pokedex  app con useffect

--- semana04/dia01-react-pokedex-app/src/App.jsx
+++ semana04/dia01-react-pokedex-app/src/App.jsx
@@ -22,12 +22,16 @@ function App() {
   }, [])
 
   return (
-    <main className="w-1/2 m-auto border border-red-700 p-6">
+    <main className="w-1/2 m-auto border p-6 mt-6">
       <h1 className="text-3xl font-bold underline text-center">Poké Catalog</h1>
       
       <section className="mt-6">
         {/* TODO: Renderizar cada nombre de los pokemons que se encuentrn en el estado pokemons   */}
         {pokemons.map(pokemon => {
+          {/* https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png */}
+          const id = pokemon.url.split('/').at(-2)
+          const pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+
           return (
             <article
               key={pokemon.url}
@@ -37,13 +41,13 @@ function App() {
 
               {/* TODO: mostrar la imagen del pokemon correspondiente que se encuentra en su objeto de detalle en la clave "sprites.other.official-artwork.front_default" */}
 
-              <img src="" className="w-40" />
+              <img src={pokemonImage} className="w-40" />
             </article>
           )
         })}
       </section>
       
-      <pre>{JSON.stringify(pokemons, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(pokemons, null, 2)}</pre> */}
 
 
       {/* <button onClick={() => setCounter(counter + 1)}>{counter}</button> */}
