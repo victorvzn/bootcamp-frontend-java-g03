144c36486027684b5038b0aacdce5bfbf22dfc78
Victor Villazón <victorzn23@gmail.com>
2024-04-24 20:03:45 -0500

    semana04, dia02, react router intro, bootstrap

--- semana04/dia02-react-router-components/src/App.jsx
+++ semana04/dia02-react-router-components/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import HomePage from './pages/Home'
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>Estoy en la pagina principal</h1>} />
-        
+        <Route path='/' element={<HomePage />} />
+
         <Route path='/login' element={<h1>LoginPage</h1>} />
         <Route path='/register' element={<h1>RegisterPage</h1>} />
       </Routes>
