7851a89c80e31640ad24b14f0d9f19159234508c
Victor Villazón <victorzn23@gmail.com>
2024-04-26 19:50:39 -0500

    semana04, dia03, react zustand intro + shopping cart

--- semana04/dia03-react-zustand-intro-shopping-cart/src/App.jsx
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/App.jsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col items-center bg-[#CD8D7A] p-4">
+      <h3 className="text-3xl">Counter with Zustand</h3>
+
+      <div className="flex items-center gap-4 mt-4">
+        <button className="p-3 bg-green-500 rounded-lg">-1</button>
+        <span className="font-bold text-4xl">0</span>
+        <button className="p-3 bg-green-500 rounded-lg">+1</button>
+      </div>
+    </div>
   )
 }
---
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/store/counter.js
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+
+export const useCounterStore = create(
+  (set) => ({
+    // Estado INICIAL
+    count: 1,
+    // Actions
+    increment: () => {
+      set(state => {
+        // Lógica adicional
+        return {
+          count: state.count + 1
+        }
+      })
+    },
+    decrement: () => {
+      set(state => ({ count: state.count - 1 }))
+    }
+  })
+)
