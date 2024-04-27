c53eac93ffd58acf692024c09058399a4fe0958f
Victor Villazón <victorzn23@gmail.com>
2024-04-26 21:58:53 -0500

    semana04, dia03, react zustand intro + shopping cart

--- semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/ProductItem.jsx
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/ProductItem.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 
 const ProductItem = ({ product }) => {
+  // DONE: Renderizar los campos de la imagen, titulo, descripción y precio en este componente.
+
   return (
     <div className="w-full border border-gray-200 rounded-lg min-h-80 shadow">
       <img
         className="rounded-lg h-48 object-cover w-full"
+        src={product.thumbnail}
       />
       <div className="px-5 pb-5 flex flex-col gap-2 mt-4">
         <h5 className="text-xl font-semibold text-gray-900">
-          title
+          {product.title}
         </h5>
         <div className="text-ellipsis overflow-hidden h-24">
-          description
+          {product.description}
         </div>
         <div className="flex flex-col items-center justify-between md:flex-row">
           <span className="text-3xl font-bold text-gray-900">
-            $0.00
+            ${product.price}
           </span>
+          
           <button
             className="bg-blue-700 hover:bg-blue-800 text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
