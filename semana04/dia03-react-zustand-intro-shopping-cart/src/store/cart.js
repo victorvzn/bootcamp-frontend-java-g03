5d24869657fa8d533f534f4c899057d880209111
Victor Villazón <victorzn23@gmail.com>
2024-04-26 23:02:22 -0500

    semana04, dia03, react zustand intro + shopping cart

--- semana04/dia03-react-zustand-intro-shopping-cart/src/store/cart.js
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/store/cart.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 
-export const useCartStore = create((set) => ({
+export const useCartStore = create((set, get) => ({
   // Estado INCIAL
   cart: [],
 
@@ -8,8 +8,27 @@ export const useCartStore = create((set) => ({
   addToCart: (newProduct) => {
     // Aquí estrá la lógica para añadir un producto al estado 'cart'
 
-    // Aquí es cuando el producto ya existe en el carrito de compras
-    // ???
+    // ¿Existe ese producto en el carrito de compras?
+    const productInCartIndex = get().cart.findIndex(
+      product => product.id === newProduct.id
+    )
+    
+    if (productInCartIndex >= 0) {
+      // Aquí es cuando el producto ya existe en el carrito de compras y solo incrementamos el campo quantity
+      const newProducts = get().cart.map(product => {
+        if (product.id === newProduct.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1
+          }
+        }
+        return product
+      })
+
+      set(state => ({ cart: newProducts }))
+
+      return
+    }
 
     // Aquí es cuando el producto es nuevo en el carrito de compras
     set(state => ({
