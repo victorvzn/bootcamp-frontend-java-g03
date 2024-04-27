c8dd3f2f8252d15e64d6df94fbbd12aa5bdf72ff
Victor Villazón <victorzn23@gmail.com>
2024-04-26 23:14:38 -0500

    semana04, dia03, react zustand intro + shopping cart

--- semana04/dia03-react-zustand-intro-shopping-cart/src/services/products.js
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/services/products.js
@@ -1,9 +1,20 @@
+import axios from 'axios'
+
 export const fetchProducts = async () => {
+  // With AXIOS
   const url = 'https://dummyjson.com/products'
 
-  const response = await fetch(url)
+  const response = await axios.get(url)
 
-  const data = await response.json()
-
-  return data.products
+  return response.data.products
 }
+
+// export const fetchProductsWithAPIFetch = async () => {
+//   const url = 'https://dummyjson.com/products'
+
+//   const response = await fetch(url)
+
+//   const data = await response.json()
+
+//   return data.products
+// }
