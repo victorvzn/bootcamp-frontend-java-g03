4ffc4791cb7d823411896d130670d239b015c16c
Victor Villazón <victorzn23@gmail.com>
2024-04-26 22:32:46 -0500

    semana04, dia03, react zustand intro + shopping cart

---
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/store/cart.js
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+
+export const useCartStore = create((set) => ({
+  // Estado INCIAL
+  cart: [],
+
+  // Actions
+  addToCart: (newProduct) => {
+    // Aquí estrá la lógica para añadir un producto al estado 'cart'
+
+    // Aquí es cuando el producto ya existe en el carrito de compras
+    // ???
+
+    // Aquí es cuando el producto es nuevo en el carrito de compras
+    set(state => ({
+      cart: [...state.cart, { ...newProduct, quantity: 1 }]
+    }))
+  },
+  removeFromCart: (id) => {},
+  cleanCart: () => {}
+}))
