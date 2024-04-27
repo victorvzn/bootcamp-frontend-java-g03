a7b11cde6fc27b04b416dd272e22aff85c00a29a
Victor Villazón <victorzn23@gmail.com>
2024-04-26 23:11:32 -0500

    semana04, dia03, react zustand intro + shopping cart

--- semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/Header.jsx
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/Header.jsx
@@ -17,8 +17,15 @@ const Header = () => {
       <div className="container mx-auto flex justify-between">
         <h1 className="font-bold">Shopping Cart</h1>
       
-        <button onClick={toggleSidebar} className="text-3xl">
+        <button onClick={toggleSidebar} className="text-3xl relative">
           <TbShoppingBag />
+          {cart.length > 0 && (
+            <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -left-6 dark:border-gray-900">
+              {/* TODO: Mostrar el total de productos sumando el campo quantity */}
+              
+              {cart.length}
+            </div>
+          )}
         </button>
 
         <div
@@ -32,17 +39,19 @@ const Header = () => {
             <button onClick={toggleSidebar}>❌</button>
           </div>
 
-          <div className="py-3">
-            <button
-              className="w-full text-slate-900 bg-red-400 hover:bg-red-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-              onClick={cleanCart}
-            >
-              Clean Cart
-            </button>
-          </div>
+          {cart.length > 0 && (
+            <div className="py-3">
+              <button
+                className="w-full text-slate-900 bg-red-400 hover:bg-red-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                onClick={cleanCart}
+              >
+                Clean Cart
+              </button>
+            </div>
+          )}
 
           <div className="flex flex-col gap-1 m-4">
-            {cart.map(product => (
+            {cart.map(product => (  
               <button
                 className="text-slate-900 bg-yellow-400 hover:bg-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                 onClick={() => removeFromCart(product.id)}
