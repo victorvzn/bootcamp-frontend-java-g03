81ce863ea777207c79143f28d209d1e65c54bcb3
Victor Villazón <victorzn23@gmail.com>
2024-04-26 22:03:55 -0500

    semana04, dia03, react zustand intro + shopping cart

--- semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/Header.jsx
+++ semana04/dia03-react-zustand-intro-shopping-cart/src/components/product-page/Header.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react"
+
 const Header = () => {
+  const [open, setOpen] = useState(false)
+
+  const classSidebar = 'fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-slate-100 w-80 text-black'
+
+  const toggleSidebar = () => setOpen(!open)
+  
   return (
     <div className="bg-lime-600 text-white py-4 px-6 w-full">
       <div className="container mx-auto flex justify-between">
         <h1 className="font-bold">Shopping Cart</h1>
       
-        <button>
+        <button onClick={toggleSidebar}>
           Cart
         </button>
 
-        <div className="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-slate-100 w-80 text-black translate-x-full">
+        <div
+          className={`${classSidebar} ${open ? 'translate-none' : 'translate-x-full'}`}
+        >
           <div className="flex justify-between">
             <h5 className="flex text-2xl font-semibold text-gray-500">
               Cart
             </h5>
 
-            <button>❌</button>
+            <button onClick={toggleSidebar}>❌</button>
           </div>
 
           <div className="py-3">
