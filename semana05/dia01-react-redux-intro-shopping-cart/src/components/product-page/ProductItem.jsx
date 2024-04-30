084ccb42eb8a9fbed9a8e68d8eb1381b3865878b
Victor Villazón <victorzn23@gmail.com>
2024-04-29 22:08:36 -0500

    semana05, dia01, react redux intro + shopping cart

--- semana05/dia01-react-redux-intro-shopping-cart/src/components/product-page/ProductItem.jsx
+++ semana05/dia01-react-redux-intro-shopping-cart/src/components/product-page/ProductItem.jsx
@@ -1,26 +1,27 @@
 import { useDispatch } from 'react-redux'
 
 import { addToCart } from '../../store/cart'
+import { formatNumber } from '../../utils'
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
   const dispatch = useDispatch()
 
   return (
     <div className="w-full border border-gray-200 rounded-lg min-h-80 shadow">
       <img
         className="rounded-lg h-48 object-cover w-full"
-        src="{product.thumbnail}"
+        src={product.thumbnail}
       />
       <div className="px-5 pb-5 flex flex-col gap-2 mt-4">
         <h5 className="text-xl font-semibold text-gray-900">
           product.title
         </h5>
         <div className="text-ellipsis overflow-hidden h-24">
-          product.description
+          {product.description}
         </div>
         <div className="flex flex-col items-center justify-between md:flex-row">
           <span className="text-3xl font-bold text-gray-900">
-            $formatNumber(product.price)
+            ${formatNumber(product.price)}
           </span>
           
           <button
