a4a8a2c8dffe64d5927812b713b821cbe09648bd
Victor Villazón <victorzn23@gmail.com>
2024-04-29 22:00:26 -0500

    semana05, dia01, react redux intro + shopping cart

--- semana05/dia01-react-redux-intro-shopping-cart/src/components/product-page/ProductList.jsx
+++ semana05/dia01-react-redux-intro-shopping-cart/src/components/product-page/ProductList.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react"
 import ProductItem from "./ProductItem"
+import { fetchProducts } from "../../services/products"
 
 const ProductList = () => {
+  const [productList, setProductList] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then(setProductList)
+  }, [])
+  
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
-      <ProductItem />
+      {productList.map(
+        product => <ProductItem
+          key={product.id}
+          product={product}
+        />
+      )}
+      
+      {/* {JSON.stringify(productList)} */}
     </div>
   )
 }
