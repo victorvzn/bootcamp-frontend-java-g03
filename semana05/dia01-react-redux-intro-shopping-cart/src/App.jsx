de122976e1b67f8e2b87b97bd6ad634269645f12
Victor Villazón <victorzn23@gmail.com>
2024-04-29 19:49:20 -0500

    semana05, dia01, react redux intro + shopping cart

--- semana05/dia01-react-redux-intro-shopping-cart/src/App.jsx
+++ semana05/dia01-react-redux-intro-shopping-cart/src/App.jsx
@@ -1,7 +1,9 @@
+import Counter from "./components/Counter.jsx";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Counter />
+    </>
   )
 }
