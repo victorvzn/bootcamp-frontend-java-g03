baf7878337ed451131d0f06a168698c7acd7bf44
Victor Villazón <victorzn23@gmail.com>
2024-04-29 21:28:32 -0500

    semana05, dia01, react redux intro + shopping cart

--- semana05/dia01-react-redux-intro-shopping-cart/src/components/product-page/ProductList.jsx
+++ semana05/dia01-react-redux-intro-shopping-cart/src/components/product-page/ProductList.jsx
@@ -1,6 +1,10 @@
+import ProductItem from "./ProductItem"
+
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+      <ProductItem />
+    </div>
   )
 }
 
