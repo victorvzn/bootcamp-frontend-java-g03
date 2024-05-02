5d1e6a8bfa00a43ee45193293faa5a715eda2ab8
Victor Villazón <victorzn23@gmail.com>
2024-05-01 20:13:24 -0500

    semana05, dia02, nextjs intro

--- semana05/dia02-nextjs-intro/app/layout.js
+++ semana05/dia02-nextjs-intro/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <nav className="flex gap-2">
+          <Link href='/'>Home</Link>
+          <Link href='/about'>About</Link>
+          <Link href='/profile'>Profile</Link>
+        </nav>
+
         {children}
       </body>
     </html>
---
+++ semana05/dia02-nextjs-intro/app/profile/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const layout = ({ children }) => {
+  return (
+    <>
+      <h2>Layout de la página profile</h2>
+      {children}
+    </>
+  )
+}
+
+export default layout
