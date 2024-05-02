1bde6a59d4fb8c21ea7db74387abfc5b92b6bb29
Victor Villazón <victorzn23@gmail.com>
2024-05-01 20:52:59 -0500

    semana05, dia02, nextjs intro

---
+++ semana05/dia02-nextjs-intro/app/characters/page.js
@@ -0,0 +1,9 @@
+const CharactersPage = () => {
+  /* TODO: reenderizar los personajes de api de rick and morty en este componente. https://rickandmortyapi.com */
+
+  return (
+    <div>CharactersPage</div>
+  )
+}
+
+export default CharactersPage
