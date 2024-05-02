3947a4c39e6f6240292c86f5b6e5f05e506b1040
Victor Villazón <victorzn23@gmail.com>
2024-05-01 20:50:34 -0500

    semana05, dia02, nextjs intro

--- semana05/dia02-nextjs-intro/app/page.js
+++ semana05/dia02-nextjs-intro/app/page.js
@@ -1,5 +1,27 @@
-export default function Home() {
+const fetchPokemons = async () => {
+  const url = 'https://pokeapi.co/api/v2/pokemon'
+
+  const response = await fetch(url)
+
+  const data = await response.json()
+
+  console.log(data)
+
+  return data.results
+}
+
+export default async function Home() {
+  const pokemons = await fetchPokemons()
+
   return (
-    <h1 className="text-3xl text-center">Hola Next.js</h1>
+    <>
+      <h1 className="text-3xl text-center">Pokemons Page</h1>
+
+      <ol>
+        {pokemons.map(pokemon => {
+          return <li key={pokemon.url}>{pokemon.name}</li>
+        })}
+      </ol>
+    </>
   );
 }
